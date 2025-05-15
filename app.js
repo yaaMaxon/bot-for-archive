979562a73a3b2ebde4f3aabaf2d6a3ad67bddf7f
Maksym <maks.sansei@gmail.com>
2025-05-15 22:36:11 +0300

    add one chacking issue

--- app.js
+++ app.js
@@ -53,6 +53,11 @@ ${message}
     });
 })
 
+//chacking is server runnig
+app.get('/', (req, res) => {
+  res.send('Bot server is running.');
+});
+
 // setInterval(() => {
 //   https.get("https://bot-for-archive.onrender.com");
 // }, 45000);
